f8fdfa7dadb9ea0ca2098fb2e7adfda1a401ac96
naveen-84 <sharmanaveens983@gmail.com>
2022-05-15 00:21:40 +0530

    cleanup changes and hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", () => {
+  let isMobile = false;
+  let isHamburgerMenuOpen = false;
 
-    $('#menu').click(function(){
-       $(this).toggleclass('fa-times');
-       $('.navbar').toggleclass('nav-toggle');
-    });
+  const toggleHeader = () => {
+    const hamburgerMenu = document.querySelector("#hamburger_menu");
+    const openHamburgerIcon = document.querySelector("#open_hamburger");
+    const closeHamburgerIcon = document.querySelector("#close_hamburger");
+    const navbar = document.querySelector("header .navbar");
+
+    const toggleHamburgerMenu = () => {
+      if (isHamburgerMenuOpen) {
+        openHamburgerIcon.style.display = "none";
+        closeHamburgerIcon.style.display = "block";
+        navbar.className = "navbar open";
+      } else {
+        openHamburgerIcon.style.display = "block";
+        closeHamburgerIcon.style.display = "none";
+        navbar.className = "navbar";
+      }
+    };
+
+    if (isMobile) {
+      hamburgerMenu.style.display = "block";
+      hamburgerMenu.addEventListener("click", () => {
+        if (isHamburgerMenuOpen) {
+          isHamburgerMenuOpen = false;
+        } else {
+          isHamburgerMenuOpen = true;
+        }
+        toggleHamburgerMenu();
+      });
+    } else {
+      hamburgerMenu.style.display = "none";
+    }
+    toggleHamburgerMenu();
+  };
+
+  const toggleIsMobile = () => {
+    if (window.innerWidth < 768) {
+      isMobile = true;
+    } else {
+      isMobile = false;
+    }
+  };
+
+  window.addEventListener("resize", (e) => {
+    toggleIsMobile();
+    toggleHeader();
+  });
+  toggleIsMobile();
+  toggleHeader();
 });
